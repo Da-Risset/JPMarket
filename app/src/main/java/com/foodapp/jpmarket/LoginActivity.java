08e0abcf148287c3c16c25faa038826b8e63152e
Arisdwi666 <aristakiya000@gmail.com>
2021-11-07 22:15:09 +0700

    scnd commit

--- app/src/main/java/com/foodapp/jpmarket/LoginActivity.java
+++ app/src/main/java/com/foodapp/jpmarket/LoginActivity.java
@@ -12,32 +12,21 @@ import android.widget.TextView;
 import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String LOG_Tag = LoginActivity.class.getSimpleName();
 
-    Button signIn;
-    EditText email,password;
-    TextView signUp;
-    private Object Intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //signIn = findViewById(R.id.login_btn);
-        //email = findViewById(R.id.email_login);//
-        //password = findViewById(R.id.password_login);//
-        //signUp = findViewById(R.id.sign_up);//
-
     }
 
-    public void daftar(View view) {
-        Log.d(LOG_Tag, "daftar onclick");
+    public void signUp(View view) {
         Intent intent = new Intent(this,RegisterActivity.class);
         startActivity(intent);
     }
 
-    public void login(View view) {
+    public void signIn(View view) {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/foodapp/jpmarket/MainActivity.java
+++ app/src/main/java/com/foodapp/jpmarket/MainActivity.java
@@ -1,14 +1,30 @@
 package com.foodapp.jpmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[]= {};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.jajanan_pasar);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+
+        PopRecAdapter myadapter = new PopRecAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myadapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
